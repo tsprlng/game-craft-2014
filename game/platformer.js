1905a4ee453b2e726b94c8ea58832e6193ebe06f
Tom Spurling <tom@tspurling.co.uk>
2014-06-07 10:23:52 +0100

    dev mode

--- game/platformer.js
+++ game/platformer.js
@@ -12,14 +12,18 @@ window.addEventListener("load",function() {
 // Set up an instance of the Quintus engine  and include
 // the Sprites, Scenes, Input and 2D module. The 2D module
 // includes the `TileLayer` class as well as the `2d` componet.
-var Q = window.Q = Quintus({audioSupported: [ 'wav','mp3','ogg' ]})
-        .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio")
-        // Maximize this game to whatever the size of the browser is
-        .setup({ maximize: true })
-        // And turn on default input controls and touch input (for UI)
-        .controls(true).touch()
-        // Enable sounds.
-        .enableSound();
+var Q = window.Q =
+  Quintus({
+    audioSupported: [ 'wav','mp3','ogg' ],
+    development: true
+  })
+    .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio")
+    // Maximize this game to whatever the size of the browser is
+    .setup({ maximize: true })
+    // And turn on default input controls and touch input (for UI)
+    .controls(true).touch()
+    // Enable sounds.
+    .enableSound();
 
 // Load and init audio files.
 

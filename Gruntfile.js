c3d90c3e502949b7c92a02b3d00c37ce42115859
Tom Spurling <tom@tspurling.co.uk>
2014-06-07 16:23:29 +0100

    Allow custom hostname

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
       options: {
         port: grunt.option('port') || 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: grunt.option('host') || 'localhost',
         // livereload: 35729,
         middleware: function (connect, options) {
           return [
